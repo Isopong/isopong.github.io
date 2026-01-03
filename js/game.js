cdf21180bdb12658cf48e7a6b92193211fa8cec3
edward (creator of isopong game) <erlkonig10@gmail.com>
2026-01-03 09:17:48 -0500

    Update game.js

--- js/game.js
+++ js/game.js
@@ -2,11 +2,26 @@ class Game {
   constructor(canvas) {
     this.ctx = canvas.getContext("2d");
 
-    this.table = new Image();
-    this.table.src = "assets/sprites/table.png";
+    // --- TABLE PHYSICS GEOMETRY ---
+    // Pixel-scaled real dimensions (tuned for 960x540)
+    this.table = {
+      centerX: 480,
+      centerY: 300,
+
+      width: 520,    // corresponds to 2.74m
+      depth: 290,    // corresponds to 1.525m
+
+      isoSkew: 0.45  // isometric Y compression
+    };
+
+    // Compute playable surface bounds (top-down space)
+    this.table.left   = this.table.centerX - this.table.width / 2;
+    this.table.right  = this.table.centerX + this.table.width / 2;
+    this.table.top    = this.table.centerY - this.table.depth / 2;
+    this.table.bottom = this.table.centerY + this.table.depth / 2;
 
     this.paddle = new Paddle(canvas);
-    this.ball = new Ball(this.paddle);
+    this.ball = new Ball(this.paddle, this.table);
 
     this.lastTime = 0;
 
@@ -41,7 +56,23 @@ class Game {
     ctx.imageSmoothingEnabled = false;
     ctx.clearRect(0, 0, 960, 540);
 
-    ctx.drawImage(this.table, 160, 80, 640, 380);
+    // --- DRAW ISOMETRIC TABLE ---
+    ctx.fillStyle = "#2e8b57"; // temporary felt green
+
+    ctx.beginPath();
+    ctx.moveTo(this.table.left, this.table.top * this.table.isoSkew);
+    ctx.lineTo(this.table.right, this.table.top * this.table.isoSkew);
+    ctx.lineTo(this.table.right, this.table.bottom * this.table.isoSkew);
+    ctx.lineTo(this.table.left, this.table.bottom * this.table.isoSkew);
+    ctx.closePath();
+    ctx.fill();
+
+    // Net (for reference)
+    ctx.strokeStyle = "#ffffff";
+    ctx.beginPath();
+    ctx.moveTo(this.table.centerX, this.table.top * this.table.isoSkew);
+    ctx.lineTo(this.table.centerX, this.table.bottom * this.table.isoSkew);
+    ctx.stroke();
 
     this.ball.draw(ctx);
     this.paddle.draw(ctx);
